390ad30be36be5d78e2387b05cd6eccb85c8ea66
Dark_Reid <apnanas@inbox.lv>
2021-12-25 18:10:43 +0300

    Created project main form. Made css_normalize

---
+++ juniortest.Denis.Vingris.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main_wall.css">
+    <title>Junior Test</title>
+</head>
+<body>
+    <article class="header_menu">
+        <header>
+            <div class="content">
+                <div class="header_panel">
+                    <div class="header_text">Product List</div>
+                    <button type="button" class="add_btn">Add</button>
+                    <button type="button" class="delete_btn">Mas Delete</button>
+                </div>
+            </div>
+        </header>
+    </article>
+</body>
+</html>
