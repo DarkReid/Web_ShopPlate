9e73b01a85b24b92747ef1e552ac007a5467284f
Dark_Reid <apnanas@inbox.lv>
2021-12-25 20:26:55 +0300

    Aded main Page and style to it, deleted sass file(may be add in the future)

--- juniortest.Denis.Vingris.php
+++ juniortest.Denis.Vingris.php
@@ -5,7 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/main_wall.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
     <title>Junior Test</title>
 </head>
 <body>
@@ -13,12 +17,29 @@
         <header>
             <div class="content">
                 <div class="header_panel">
-                    <div class="header_text">Product List</div>
-                    <button type="button" class="add_btn">Add</button>
-                    <button type="button" class="delete_btn">Mas Delete</button>
+                    <div class="header_text-size">
+                        <div class="header_text">Product List</div>
+                    </div>
+                    <div class="header_btn">
+                        <button type="button" class="add_btn">Add</button>
+                        <button type="button" class="delete_btn">Mas Delete</button>
+                    </div>
                 </div>
+                <hr>
             </div>
         </header>
+    </article>
+    <article class="footer">
+        <footer>
+            <div class="content">
+                <div class="footer_panel">
+                    <hr>
+                    <div class="footer_text-size">
+                        <div class="footer_text">Scandiweb Test Assigment</div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </article>
 </body>
 </html>
