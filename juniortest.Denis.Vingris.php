e9c079e73925348e8e97e96df2fe16573f39a865
Dark_Reid <apnanas@inbox.lv>
2022-01-15 21:47:36 +0300

    Add adaptive at pages

--- juniortest.Denis.Vingris.php
+++ juniortest.Denis.Vingris.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/main_wall.css">
+    <link rel="stylesheet" href="css/media.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
@@ -32,6 +33,28 @@
     <div class="product_list">
         <div class="content">
             <div class="article">
+                <div class="list">
+                    <label class="container">
+                        <input type= "checkbox" checked="">
+                    </label>
+                    <div class="product">
+                        <div class="type">id</div>
+                        <div class="type">name</div>
+                        <div class="type">price</div>
+                        <div class="type">description</div>
+                    </div>
+                </div>
+                <div class="list">
+                    <label class="container">
+                        <input type= "checkbox" checked="">
+                    </label>
+                    <div class="product">
+                        <div class="type">id</div>
+                        <div class="type">name</div>
+                        <div class="type">price</div>
+                        <div class="type">description</div>
+                    </div>
+                </div>
                 <div class="list">
                     <label class="container">
                         <input type= "checkbox" checked="">
